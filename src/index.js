7f3f40a9e23fef8cb87af14eea79377f7c3b80a4
M-Rasul <rasulmamashev2@gmail.com>
2023-01-03 14:28:37 +0500

    feat: create algorithm for reversing a number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) {
+    n = n * -1;
+  }
+  const stringNum = '' + n;
+  const arr = stringNum.split('');
+  const newArr = [];
+  arr.forEach(num => newArr.unshift(num));
+  const reverseString = newArr.join('');
+  return +reverseString;
 }
